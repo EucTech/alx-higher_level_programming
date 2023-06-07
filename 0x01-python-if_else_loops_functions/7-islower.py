7850ae35491196cc97d3fdaab29f84988cac9ea9
EucTech <euccreative@gmail.com>
2023-06-07 11:47:00 +0000

    Created a script that prints lower or upper letters

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    for lower in range(ord('a'), ord('z') + 1):
-        if c == chr(lower):
+    for lower in c:
+        if ord(c) >= 97 and ord(c) <= 122:
             return True
     return False
