912fbfa8322dfef5528e18c0b7575f29922211e9
EucTech <euccreative@gmail.com>
2023-06-07 11:53:41 +0000

    handled empty string

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if not c:
-        return False
     for lower in c:
-        if ord(c) >= 97 and ord(c) <= 122:
+        if ord(lower) >= 97 and ord(lower) <= 122:
             return True
     return False
