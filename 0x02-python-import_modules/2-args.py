fe9a626e17e2a84aac80be683a67c9bd42cb4e0c
EucTech <euccreative@gmail.com>
2023-06-08 19:40:13 +0000

    fixed eror

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,11 @@ import sys
 length = len(sys.argv)-1
 count = 0
 if length == 0:
-    print("0 arguments.")
+    print("{} arguments.".format(length))
 elif length == 1:
-    print("1 argument:")
-    for i in range(1, length):
-        print("{}: {}".format(length, sys.argv[i]))
+    print("{} argument:".format(length))
+    for i in range(1, length + 1):
+        print("{}: {}".format(i, sys.argv[i]))
 elif length > 1:
     print("{} arguments:".format(length))
     for i in range(1, length + 1):
