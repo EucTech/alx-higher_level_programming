39a5fd8fe80b43e1dbb73f445fec4f09d5ec63e7
EucTech <euccreative@gmail.com>
2023-06-08 19:34:17 +0000

    created a script that takes a command line arguent

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+length = len(sys.argv)-1
+count = 0
+if length == 0:
+        print("0 arguments.")
+elif length == 1:
+    print("1 argument:")
+    for i in range(1, length):
+        print("{}: {}".format(length, sys.argv[i]))
+elif length > 1:
+    print("{} arguments:".format(length))
+    for i in range(1, length + 1):
+        print("{}: {}".format(i, sys.argv[i]))
