8f76ce3c890c9b7a796164a9debcc11ab436f824
EucTech <euccreative@gmail.com>
2023-06-08 19:53:18 +0000

    fixed pycodestyle eror

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import sys
-if __name__  == "__main__":
+if __name__ == "__main__":
     length = len(sys.argv)-1
     if length == 0:
         print("{} arguments.".format(length))
