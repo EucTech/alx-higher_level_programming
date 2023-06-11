a0d91a37b25537f37f59b2d0a0ed485d5c6971b8
EucTech <euccreative@gmail.com>
2023-06-11 21:06:40 +0000

    created a script that print an element in reverse order

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,4 @@
 def print_reversed_list_integer(my_list=[]):
     for cnt in range(len(my_list)-1, -1, -1):
         if my_list[cnt] != 0:
-            print("{}".format(my_list[cnt]))
+            print("{:d}".format(my_list[cnt]))
