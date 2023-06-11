ba8fb4e3159f0529c62fa180a0f282309b97885b
EucTech <euccreative@gmail.com>
2023-06-11 19:41:29 +0000

    created a script that retrieves an element from a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,11 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    elif idx > len(my_list):
+    elif idx >= len(my_list):
         return None
     else:
-        return my_list[idx]
+        count = 0
+        for i in my_list:
+            if count == idx:
+                return i
+            count += 1
