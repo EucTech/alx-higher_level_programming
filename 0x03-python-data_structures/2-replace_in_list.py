8de27eae8b7bc64809fc64042823d5017234b122
EucTech <euccreative@gmail.com>
2023-06-11 20:19:30 +0000

    created a script that replaces a an element in a given index

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -9,5 +9,5 @@ def replace_in_list(my_list, idx, element):
         for i in my_list:
             if count == idx:
                 my_list[count] = element
-                return my_list
             count += 1
+        return my_list
