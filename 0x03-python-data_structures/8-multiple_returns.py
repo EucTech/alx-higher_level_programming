00fc2a019968b67cac121365ce28fe7e2d6f7dcf
EucTech <euccreative@gmail.com>
2023-06-12 11:00:26 +0000

    creared a script that prints the length and first character of a string

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     length = len(sentence)
     for i in sentence:
-        if i == " ":
+        if i == "":
             i[0] = None
         else:
             return length, i[0]
