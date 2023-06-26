f0fa5b0ae64c91db0c852d1f0e92e402073a0ca5
EucTech <euccreative@gmail.com>
2023-06-26 20:46:17 +0000

    fixed excepttion

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,6 +8,6 @@ def safe_print_list(my_list=[], x=0):
             if val == x:
                 break
         print()
-    except (TypeError, ValueError):
+    except IndexError:
         pass
     return val
