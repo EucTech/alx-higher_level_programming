d39a8e690cc762216d8b80c5bc2624ddb6c78dd6
EucTech <euccreative@gmail.com>
2023-06-26 22:31:59 +0000

    printed an integer

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    length = 0
+    try:
+        for i in range(x):
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end="")
+                length += 1
+        print()
+    except IndexError:
+        pass
+        print()
+    return length
