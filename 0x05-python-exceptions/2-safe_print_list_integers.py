ddb859f65d4ee4c5fc3dab1ea426315cb9b34fed
EucTech <euccreative@gmail.com>
2023-06-26 22:48:28 +0000

    fixed error

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
                 print("{:d}".format(my_list[i]), end="")
                 length += 1
         print()
-    except IndexError:
+    except (IndexError, TypeError):
         pass
         print()
     return length
