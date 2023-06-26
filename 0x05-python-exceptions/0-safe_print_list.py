7f434aa670f7f67d346481a7373328dcf14ec124
EucTech <euccreative@gmail.com>
2023-06-26 20:43:49 +0000

    created a script that prints x elements of a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         val = 0
         for ele in my_list:
-            print(ele, end="")
+            print("{}".format(ele), end="")
             val += 1
             if val == x:
                 break
