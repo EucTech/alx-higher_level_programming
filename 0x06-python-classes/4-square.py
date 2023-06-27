9a9d606938431ac8f55c9752f5ad716ffae28b07
EucTech <euccreative@gmail.com>
2023-06-27 23:31:34 +0000

    tash 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,7 +5,15 @@
 class Square:
     """This is a function that defines a square"""
     def __init__(self, size=0):
-        self.size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size ** 2
 
     @property
     def size(self):
@@ -15,10 +23,7 @@ class Square:
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
-
-    def area(self):
-        return self.__size ** 2
