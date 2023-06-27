a6b0a8eca7d95556d1a052119a6f3d5e0a107d2a
EucTech <euccreative@gmail.com>
2023-06-27 20:25:57 +0000

    fixed erro

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" This is a function for square class"""
+
+
 class Square:
-"""This is a functon that print empty class"""
+    """This is a functon that print empty class"""
     pass
