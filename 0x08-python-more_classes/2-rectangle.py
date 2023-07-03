2369c38ee24244370f1df5c58ae1eaab955aed1e
EucTech <euccreative@gmail.com>
2023-07-03 15:04:16 +0000

    fixed error

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -49,7 +49,5 @@ class Rectangle:
     def perimeter(self):
         return (self.width * 2) + (self.height * 2)
 
-        if self.width or self.height == 0:
-            return 0
-        else:
-            return (self.width * 2) + (self.height * 2)
+        if n in self.width or self.height == 0:
+            n = 0
