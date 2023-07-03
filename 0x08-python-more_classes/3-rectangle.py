d82a9541f3e4e419113cde46725028dca9bba892
EucTech <euccreative@gmail.com>
2023-07-03 17:06:58 +0000

    print the rectangle with the character #

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""This is rectangle class to get the area and primeter of a rectangle"""
+
+
+class Rectangle:
+    """This is the definion of the class"""
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width * 2) + (self.__height * 2)
+
+    def __str__(self):
+        rec_list = ""
+        for i in range(self.__height):
+            rec_list += "#" * self.__width + "\n"
+        return rec_list
