6737f44d9c1bd06b1f3bf8474513edffdf4a9363
EucTech <euccreative@gmail.com>
2023-07-09 11:05:57 +0000

    fixed code

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -6,10 +6,11 @@ def text_indentation(text):
     """This is the function that prints text_indentation"""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    if '. ' in text:
-        text = text.replace('. ', '.\n\n')
-    if '? ' in text:
-        text = text.replace('? ', '?\n\n')
-    if ': ' in text:
-        text = text.replace(': ', ':\n\n')
+    if '.' in text:
+        text = text.replace('.', '.\n\n')
+    if '?' in text:
+        text = text.replace('?', '?\n\n')
+    if ':' in text:
+        text = text.replace(':', ':\n\n')
+    text = "\n".join(line.lstrip() for line in text.strip().split("\n"))
     print(text)
