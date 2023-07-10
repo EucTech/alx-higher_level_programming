8b24eda4f514af76f7c2f433c83d63c49afc6901
EucTech <euccreative@gmail.com>
2023-07-10 18:23:00 +0000

    added doctest

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,19 +1,7 @@
 #!/usr/python3
 """ a class MyList that inherits from list"""
 
-class MyList:
-    def __init__(self, lists=[]):
-        self.lists = lists
-    
-    def append(self, value):
-        if isinstance(value, int):
-            self.lists.append(value)
-        else:
-            raise ValueError("lists must integer")
-
+class MyList(list):
     def print_sorted(self):
-        result = sorted(self.lists)
+        result = sorted(self)
         print(result)
-
-    def __str__(self):
-        return str(self.lists)
