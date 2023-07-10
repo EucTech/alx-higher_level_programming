faf35a8c3f5ace43b75b4b296358cf3149ff4df9
EucTech <euccreative@gmail.com>
2023-07-10 18:26:24 +0000

    fixed pycodestyle

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,7 +1,9 @@
 #!/usr/python3
 """ a class MyList that inherits from list"""
 
+
 class MyList(list):
+    """mylist function"""
     def print_sorted(self):
         result = sorted(self)
         print(result)
