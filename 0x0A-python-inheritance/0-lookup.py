7d5cff41fc9c149b3d8a491606a2358006992cb2
EucTech <euccreative@gmail.com>
2023-07-10 16:27:47 +0000

    a function that returns the list of the available attributes and memebers of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    This is a function that returns the list of available
+    attributes and methods of an object
+"""
+
+
+def lookup(obj):
+    """function that returns dir()`"""
+    return dir(obj)
