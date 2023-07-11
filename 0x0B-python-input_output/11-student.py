0ce1a4a666d368b04ec10195cc6126ccb2d418d6
EucTech <euccreative@gmail.com>
2023-07-11 23:32:59 +0000

    11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    This is a  class Student that defines a student
+"""
+
+
+class Student:
+    """Class student"""
+    def __init__(self, first_name, last_name, age):
+        """Instantiation of class attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """To return dicitonary repr of students"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {x: getattr(self, x) for x in attrs if hasattr(self, x)}
+
+    def reload_from_json(self, json):
+        """"to replaces all attributes of the Student instance"""
+        for key in json:
+            if hasattr(self, key):
+                setattr(self, key, json[key])
