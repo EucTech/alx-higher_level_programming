32ee5b58e5686f900ad4dee431e48982b1c94fa9
EucTech <euccreative@gmail.com>
2023-07-11 15:13:16 +0000

    a script that opens and reads a file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+     This is a function that reads a text file (UTF8)
+    and prints it to stdout
+"""
+
+
+def read_file(filename=""):
+    """This is the read file"""
+    with open(filename, encoding="utf-8") as f:
+        read_f = f.read()
+    print(read_f)
