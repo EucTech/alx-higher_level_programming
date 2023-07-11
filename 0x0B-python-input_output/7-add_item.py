ca6d80e5f4ed5fd5546c7d0ff9ccbbb3ee6f5f99
EucTech <euccreative@gmail.com>
2023-07-11 21:32:32 +0000

    a script that adds all arguments to a Python list, and then save them to a file:

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    This is  a script that adds all arguments to a Python
+    list, and then save them to a file
+"""
+import json
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+arguments = sys.argv[1:]
+lists = []
+
+lists.extend(arguments)
+
+save_to_json_file(lists, "add_item.json")
+
+load_from_json_file("add_item.json")
