ba40b230e5f961c463050ab897043661b7c5042b
EucTech <euccreative@gmail.com>
2023-07-16 20:50:54 +0000

    updated a static method of json that returns JSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
 """The BASE CLASS"""
+import json
 
 
 class Base:
     """Base"""
     __nb_objects = 0
+
     def __init__(self, id=None):
         """instantiation"""
         if id is not None:
-           self.id = id
+            self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """To return json string representation"""
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,8 +1,12 @@
 import unittest
 from models.base import Base
-
+from models.rectangle import Rectangle
+from models.square import Square
 
 class TestBase(unittest.TestCase):
+
+    def setUp(self):
+        Base._Base__nb_objects = 0
 
     def test__init__(self):
         b = Base()
@@ -21,3 +25,28 @@ class TestBase(unittest.TestCase):
         self.assertEqual(b.id, 'hello')
         b = Base(0)
         self.assertEqual(b.id, 0)
+
+    def test_to_json_string(self):
+        r = Rectangle(10, 7, 2, 8)
+        dic = r.to_dictionary()
+        d = Base.to_json_string([dic])
+        res = '[{"id": 1, "width": 10, "height": 7, "x": 2, "y": 8}]'
+        self.assertEqual(d, res)
+
+        r = Rectangle(10, 2, 8)
+        dic = r.to_dictionary()
+        d = Base.to_json_string([dic])
+        res = '[{"id": 2, "width": 10, "height": 2, "x": 8, "y": 0}]'
+        self.assertEqual(d, res)
+
+        r = Rectangle(10, 7)
+        dic = r.to_dictionary()
+        d = Base.to_json_string([dic])
+        res = '[{"id": 3, "width": 10, "height": 7, "x": 0, "y": 0}]'
+        self.assertEqual(d, res)
+
+        r = Rectangle(12, 6, 7, 2, 8)
+        dic = r.to_dictionary()
+        d = Base.to_json_string([dic])
+        res = '[{"id": 8, "width": 12, "height": 6, "x": 7, "y": 2}]'
+        self.assertEqual(d, res)
