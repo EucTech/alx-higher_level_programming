73b4774e232f32032871f6a10181d5a46dfda56f
EucTech <euccreative@gmail.com>
2023-07-17 15:33:36 +0000

    added a function that  returns the list of the JSON string representation json_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,3 +22,33 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+            To write JSON string representaion of
+            list_objs to file
+        """
+        filename = cls.__name__ + ".json"
+        llist = []
+        if list_objs is None:
+            with open(filename, 'w') as f:
+                f.write("[]")
+        elif list_objs is not None:
+            for obj in range(len(list_objs)):
+                llist.append(list_objs[obj].to_dictionary())
+
+            j_list = cls.to_json_string(llist)
+            with open(filename, 'w', encoding="utf-8") as f:
+                f.write(j_list)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+            that returns the list of the JSON string
+            representation json_string
+        """
+        if json_string is None:
+            return "[]"
+        else:
+            return json.loads(json_string)
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,7 +1,9 @@
+import os
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
+
 
 class TestBase(unittest.TestCase):
 
@@ -50,3 +52,91 @@ class TestBase(unittest.TestCase):
         d = Base.to_json_string([dic])
         res = '[{"id": 8, "width": 12, "height": 6, "x": 7, "y": 2}]'
         self.assertEqual(d, res)
+
+        r = Rectangle(6, 8)
+        d = Base.to_json_string([])
+        res = '[]'
+        self.assertEqual(d, res)
+
+        r = Rectangle(6, 8)
+        d = Base.to_json_string(None)
+        res = '[]'
+        self.assertEqual(d, res)
+
+    def test_save_file_Empty(self):
+        Rectangle.save_to_file([])
+        with open("Rectangle.json", "r") as f:
+            self.assertEqual(f.read(), "[]")
+
+    def test_save_file_None(self):
+        Rectangle.save_to_file(None)
+        with open("Rectangle.json", "r") as f:
+            self.assertEqual(f.read(), "[]")
+
+    def test_save_file_two(self):
+        r1 = Rectangle(4, 5)
+        Rectangle.save_to_file([r1])
+        res = '[{"id": 1, "width": 4, "height": 5, "x": 0, "y": 0}]'
+        with open("Rectangle.json", "r") as f:
+            self.assertEqual(f.read(), res)
+
+    def test_save_file_3(self):
+        r1 = Rectangle(4, 5, 6)
+        Rectangle.save_to_file([r1])
+        res = '[{"id": 1, "width": 4, "height": 5, "x": 6, "y": 0}]'
+        with open("Rectangle.json", "r") as f:
+            self.assertEqual(f.read(), res)
+
+    def test_save_file_two(self):
+        r1 = Rectangle(4, 5, 6, 7)
+        Rectangle.save_to_file([r1])
+        res = '[{"id": 1, "width": 4, "height": 5, "x": 6, "y": 7}]'
+        with open("Rectangle.json", "r") as f:
+            self.assertEqual(f.read(), res)
+
+    def test_save_file_two(self):
+        r1 = Rectangle(4, 5, 5, 7, 8)
+        Rectangle.save_to_file([r1])
+        res = '[{"id": 8, "width": 4, "height": 5, "x": 5, "y": 7}]'
+        with open("Rectangle.json", "r") as f:
+            self.assertEqual(f.read(), res)
+
+    def test_from_json_string(self):
+        list_input = [
+            {'id': 89, 'width': 10, 'height': 4},
+            {'id': 7, 'width': 1, 'height': 7}
+        ]
+        json_list_input = Rectangle.to_json_string(list_input)
+        list_output = Rectangle.from_json_string(json_list_input)
+        res = [
+            {'id': 89, 'width': 10, 'height': 4},
+            {'id': 7, 'width': 1, 'height': 7}
+        ]
+        self.assertEqual(list_output, res)
+
+    def test_from_json_string1(self):
+        list_input = [
+            {'id': 89, 'width': 10, 'height': 4}
+        ]
+        json_list_input = Rectangle.to_json_string(list_input)
+        list_output = Rectangle.from_json_string(json_list_input)
+        res = [
+            {'id': 89, 'width': 10, 'height': 4}
+        ]
+        self.assertEqual(list_output, res)
+
+    def test_from_json_string2(self):
+        list_input = []
+        json_list_input = Rectangle.to_json_string(list_input)
+        list_output = Rectangle.from_json_string(json_list_input)
+        res = []
+        self.assertEqual(list_output, res)
+
+    def test_from_json_string4(self):
+        list_input = [
+            {'id': 89, 'width': 10, 'height': 4},
+            {'id': 7, 'width': 1, 'height': 7}
+        ]
+        json_list_input = Rectangle.to_json_string(None)
+        list_output = Rectangle.from_json_string(json_list_input)
+        self.assertEqual(list_output, []) 
