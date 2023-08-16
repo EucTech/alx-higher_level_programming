e1738ec8a6a8c280e0b34c53716958c45b7186e4
EucTech <euccreative@gmail.com>
2023-08-15 17:31:23 -0700

    created a table called secon_table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -6,6 +6,9 @@ CREATE TABLE IF NOT EXISTS second_table (
 	score INT
 );
 
-INSERT INTO second_table
-	VALUES (1, "John", 10), (2, "Alex", 3),
-			(3, "Bod", 14), (4, "George", 8);
+INSERT INTO second_table (id, name, score)
+VALUES 
+	(1, "John", 10),
+	(2, "Alex", 3),
+	(3, "Bod", 14),
+	(4, "George", 8);
