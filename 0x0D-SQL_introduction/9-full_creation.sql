29e4bf012bfa817ab580454ce35a7763005d7d92
EucTech <euccreative@gmail.com>
2023-08-18 09:47:46 +0100

    fixed errror

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS second_table (
 	score INT
 );
 
-INSERT INTO second_table VALUES 
+INSERT INTO second_table VALUES (id, name, score)
 	(1, "John", 10),
 	(2, "Alex", 3),
 	(3, "Bod", 14),
