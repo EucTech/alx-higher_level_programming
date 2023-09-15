e139c0b74bd197896aba0de2c7fe6e33edf78e08
EucTech <euccreative@gmail.com>
2023-09-15 16:39:09 +0100

    a script that lists all states from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+     a script that lists all states from the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+        host="localhost",
+        port= 3306,
+        user="root",
+        password="euctech",
+        db="hbtn_0e_0_usa"
+    )
+
+    # cursor for excuting queries
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    result = cursor.fetchall()
+
+    for row in result:
+        print(row)
+
+    cursor.close()
+    conn.close()
