dec96ede495654ea22ce1d5943b319fd95f88d9f
EucTech <euccreative@gmail.com>
2023-09-15 17:08:55 +0100

    fixed code

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
 """
-     a script that lists all states from the database hbtn_0e_0_usa
+    a script that lists all states from the database hbtn_0e_0_usa
 """
 
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
+
+    username = sys.argv[1]
+    pwd = sys.argv[2]
+    db_name = sys.argv[3]
+
     conn = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user="root",
-        password="euctech",
-        db="hbtn_0e_0_usa"
+        user=username,
+        password=pwd,
+        db=db_name
     )
 
     # cursor for excuting queries
