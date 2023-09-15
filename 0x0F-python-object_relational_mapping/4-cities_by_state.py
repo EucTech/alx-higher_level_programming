f7bd46a733bf3e7df0dbae7f65d3b8ade0f7a1c8
EucTech <euccreative@gmail.com>
2023-09-15 23:30:29 +0100

    a script that lists all cities from the database hbtn_0e_4_usa

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Write a script that lists all cities from the
+database hbtn_0e_4_usa"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    pwd = sys.argv[2]
+    db_name = sys.argv[3]
+
+    conn = MySQLdb.connect(host="localhost", user=username, passwd=pwd,
+                           db=db_name, port=3306)
+
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM cities ORDER BY id ASC;")
+
+    states = cursor.fetchall()
+
+    for row in states:
+        print(row)
+
+    cursor.close()
+    conn.close()
