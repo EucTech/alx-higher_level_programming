7982c421f2619f3018ff07da4ba9fb20c9e7e54d
EucTech <euccreative@gmail.com>
2023-09-16 18:39:16 +0100

    fixed error

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,15 +9,15 @@ from model_state import Base, State
 
 
 if __name__ == '__main__':
-        db_url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-                sys.argv[1], sys.argv[2], sys.argv[3])
+    db_url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.\
+        format(sys.argv[1], sys.argv[2], sys.argv[3])
 
-        engine = create_engine(db_url, pool_pre_ping=True)
+    engine = create_engine(db_url, pool_pre_ping=True)
 
-        Session = sessionmaker(bind=engine)
-        session = Session()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-        result = session.query(State).order_by(State.id).all()
+    result = session.query(State).order_by(State.id).all()
 
-        for state in result:
-                print("{}: {}".format(state.id, state.name))
+    for state in result:
+        print("{}: {}".format(state.id, state.name))
