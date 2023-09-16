dbdbef0cec7238dc27c2e0d9281b5f31416fb482
EucTech <euccreative@gmail.com>
2023-09-16 18:16:26 +0100

    a script that lists all State objects from the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" a script that lists all State objects from the
+database hbtn_0e_6_usa"""
+
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+db_url = 'mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3])
+
+engine = create_engine(db_url, pool_pre_ping=True)
+
+Base.metadata.create_all(bind=engine)
+
+Session = sessionmaker(bind=engine)
+Session = Session()
+
+result = Session.query(State).order_by(State.id).all()
+
+for state in result:
+    print("{}: {}".format(state.id, state.name))
+
+Session.close()
