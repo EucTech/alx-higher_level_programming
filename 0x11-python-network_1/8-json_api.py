dd3d8acff31863966a6404a8eb8e710efe57a386
EucTech <euccreative@gmail.com>
2023-10-01 19:24:38 -0700

    8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""This is a a Python script that takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter."""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        q = ""
+    else:
+        q = sys.argv[1]
+    
+    url = 'http://0.0.0.0:5000/search_user'
+
+    data = {'q': q}
+
+    try:
+        r = requests.post(url, data)
+        r = r.json()
+
+        if r:
+            getId = r.get("id")
+            getName = r.get("name")
+            print(f"[{getId}] {getName}")
+        else:
+            print("No result")
+    except ValueError as e:
+        print("Not a valid JSON")
