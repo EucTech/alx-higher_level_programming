60bbb38c1a9dfd9615a1aea02beeb5ec6eb7a8ee
EucTech <euccreative@gmail.com>
2023-10-24 17:25:26 -0700

    1-writeme.js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const filename = process.argv[2];
+const text = process.argv[3];
+
+fs.writeFile(filename, text, 'utf8', (error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(filename);
+  }
+});
